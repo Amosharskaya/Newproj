df081fa94b368eaef48e3cccfe49fbd6ea9fa7a3
Anastasia Mosharskaya <git.user.lemeseva@gmail.com>
2025-05-19 09:48:35 +0300

    Доработка логики 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
         method1();
-        System.out.println("GIT welcome!");
+        System.out.println("GIT Welcome!");
         System.out.printf("The END!");
     }
 
